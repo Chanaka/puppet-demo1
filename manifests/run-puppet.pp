d1d88df96979e1e1cf3f190bfbf31e46c29d6990
Chanaka <pe.chanaka.ck@gmail.com>
2020-01-15 20:09:29 +0530

    say hello with exec

--- manifests/run-puppet.pp
+++ manifests/run-puppet.pp
@@ -13,3 +13,9 @@ cron { 'run-puppet':
 file { '/tmp/hello.txt':
   content => "hello world"
 }
+
+exec { 'say_hello':
+  command => '/bin/echo This is `whoami` > /tmp/say_hello.txt',
+  user    => 'ubuntu',
+  creates => '/tmp/say_hello.txt'
+}
